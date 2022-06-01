b16fc394bd391da79c74371a5a7870da51faee62
Sahar Abdel Samad <sahar.abdelsamad@gmail.com>
2022-06-01 16:19:08 +0300

    Create App and greeting component

---
+++ src/components/Greeting.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { useSelector } from "react-redux";
+
+const Greeting = () => {
+  const greeting = useSelector((state) => state.greeting);
+
+  return (
+    <h1> { greeting } </h1>
+  );
+};
+
+export default Greeting
